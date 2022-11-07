1de0be2d256a1072bf420013fb7264a67fd88cb9
Skylar Wurster <swwurster@gmail.com>
2022-11-07 10:24:44 -0500

    New results

--- Code/temp.py
+++ Code/temp.py
@@ -6,6 +6,8 @@ import time
 import subprocess
 import shlex
 from Other.utility_functions import create_path, nc_to_tensor, tensor_to_cdf
+import h5py
+import numpy as np
 
 project_folder_path = os.path.dirname(os.path.abspath(__file__))
 project_folder_path = os.path.join(project_folder_path, "..")
@@ -16,9 +18,9 @@ save_folder = os.path.join(project_folder_path, "SavedModels")
 
 if __name__ == '__main__':
     
-    d = nc_to_tensor(os.path.join(data_folder, "Isotropic.nc"))
-    d = torch.linalg.norm(d,dim=1).unsqueeze(0)
+    d = h5py.File(os.path.join(data_folder, "Supernova.h5"))
+    d = np.array(d['data'])
     print(d.shape)
-    tensor_to_cdf(d, os.path.join(data_folder, "IsotropicMag.nc"))
-
+    d = torch.tensor(d).unsqueeze(0)
+    tensor_to_cdf(d, os.path.join(data_folder, "Supernova.nc"))
     quit()
