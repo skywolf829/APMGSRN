c8d20a8f8139a8f76e8a94f0a179e637d99fde34
Skylar Wurster <swwurster@gmail.com>
2023-02-02 01:34:48 -0500

    no rotation

--- Code/Models/AMGSRN.py
+++ Code/Models/AMGSRN.py
@@ -165,8 +165,8 @@ class AMG_encoder(nn.Module):
             #self.grid_rotations.uniform_(-torch.pi/16, torch.pi/16)
             d = self.transformation_matrices.device
             self.transformation_matrices[:] = torch.eye(4, device=d, dtype=torch.float32)
-            self.transformation_matrices[:,0:3,:] += torch.rand_like(
-                self.transformation_matrices[:,0:3,:],
+            self.transformation_matrices[:,0:3,3] += torch.rand_like(
+                self.transformation_matrices[:,0:3,3],
                 device=d, dtype=torch.float32) * 0.1
             self.transformation_matrices = torch.nn.Parameter(
                 self.transformation_matrices @ \
