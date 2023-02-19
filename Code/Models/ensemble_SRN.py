4322ec9a8fbbeaf05ef41cff029cf5d9a745b113
Skylar Wurster <swwurster@gmail.com>
2023-02-19 15:45:37 -0500

    Update ensemble_SRN.py

--- Code/Models/ensemble_SRN.py
+++ Code/Models/ensemble_SRN.py
@@ -64,7 +64,6 @@ class Ensemble_SRN(nn.Module):
         indices = (x+1)/2
         indices = indices*self.model_grid_shape
         indices = indices.type(torch.int)
-        indices = indices.flip(-1)
         indices = indices[:,0] + indices[:,1]*self.model_grid_shape[0] + \
             indices[:,2]*(self.model_grid_shape[0]*self.model_grid_shape[1])
 
