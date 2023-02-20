c5ed4441aeb9ccc3c0c1f5706a1a40feb85c89fe
Skylar Wurster <swwurster@gmail.com>
2023-02-19 21:14:02 -0500

    Corrects ensemble model inference

--- Code/Models/ensemble_SRN.py
+++ Code/Models/ensemble_SRN.py
@@ -22,51 +22,68 @@ class Ensemble_SRN(nn.Module):
     def __init__(self, opt):
         super().__init__()
         self.opt = opt
+        ensemble_grid = [eval(i) for i in opt['ensemble_grid'].split(',')]
         full_shape = get_data_size(os.path.join(data_folder, opt['data']))
+        steps = [full_shape[0] / ensemble_grid[0], 
+            full_shape[1] / ensemble_grid[1],
+            full_shape[2] / ensemble_grid[2]]
+        
         models = []
+        unsorted_model_dict = {}
         local_extents = []
-        for submodel in os.listdir(os.path.join(save_folder, opt['save_name'])):
+        model_folders = os.listdir(os.path.join(save_folder, opt['save_name']))
+        model_folders.sort()
+        for submodel in model_folders:
             if(os.path.isdir(os.path.join(save_folder, opt['save_name'], submodel))):
                 sub_opt = load_options(os.path.join(save_folder, 
                     opt['save_name'], submodel))
                 sub_opt['device'] = opt['device']
-                models.append(load_model(sub_opt, opt['device']))
+                submodel_model = load_model(sub_opt, opt['device'])
                 model_extents = sub_opt['extents']
                 model_extents = [float(i) for i in model_extents.split(',')]
+                ind_1 = int(model_extents[0] / steps[0])
+                ind_2 = int(model_extents[2] / steps[1])
+                ind_3 = int(model_extents[4] / steps[2])
+                ind = ind_1 + ensemble_grid[0]*ind_2 + ind_3*ensemble_grid[0]*ensemble_grid[1]
                 model_extents[0] = ((model_extents[0] / full_shape[0]) - 0.5) * 2
                 model_extents[1] = ((model_extents[1] / full_shape[0]) - 0.5) * 2
                 model_extents[2] = ((model_extents[2] / full_shape[1]) - 0.5) * 2
                 model_extents[3] = ((model_extents[3] / full_shape[1]) - 0.5) * 2
                 model_extents[4] = ((model_extents[4] / full_shape[2]) - 0.5) * 2
                 model_extents[5] = ((model_extents[5] / full_shape[2]) - 0.5) * 2
-                local_extents.append(model_extents)
+                unsorted_model_dict[ind] = [submodel_model, model_extents]
+        print(unsorted_model_dict.keys())
+        for i in range(ensemble_grid[0]*ensemble_grid[1]*ensemble_grid[2]):
+            models.append(unsorted_model_dict[i][0])
+            local_extents.append(unsorted_model_dict[i][1])
+        
         local_extents = torch.tensor(local_extents)
 
-        ensemble_grid = [eval(i) for i in opt['ensemble_grid'].split(',')]
         print(f"Loaded {len(models)} models in ensemble model")
 
         self.register_buffer("model_grid_shape",
-            torch.tensor(ensemble_grid, dtype=torch.long).flip(-1),
+            torch.tensor(ensemble_grid, dtype=torch.long),
             persistent=False)
         self.register_buffer("full_data_shape",
             torch.tensor(full_shape, dtype=torch.long),
             persistent=False)
         self.register_buffer("local_min_extents",
-            torch.tensor(local_extents[:,0::2], dtype=torch.float32),
+            local_extents[:,0::2].clone().detach().flip(-1),
             persistent=False)
         self.register_buffer("local_max_extents",
-            torch.tensor(local_extents[:,1::2], dtype=torch.float32),
+            local_extents[:,1::2].clone().detach().flip(-1),
             persistent=False)
         self.models = torch.nn.ModuleList(models)
 
+
     def forward(self, x):    
 
-        indices = (x+1)/2
-        indices = indices*self.model_grid_shape
-        indices = indices.type(torch.int)
+        indices = (x+1)/(2+1e-6)
+        indices = indices.flip(-1)*self.model_grid_shape 
+        indices = indices.type(torch.long)
         indices = indices[:,0] + indices[:,1]*self.model_grid_shape[0] + \
             indices[:,2]*(self.model_grid_shape[0]*self.model_grid_shape[1])
-
+       
         y = torch.empty([x.shape[0], 1], 
             device=x.device, dtype=x.dtype)
 
--- Code/Models/models.py
+++ Code/Models/models.py
@@ -7,12 +7,87 @@ import os
 from math import pi
 from Models.options import *
 from Other.utility_functions import create_folder, make_coord_grid
+import math
 
 project_folder_path = os.path.dirname(os.path.abspath(__file__))
 project_folder_path = os.path.join(project_folder_path, "..", "..")
 data_folder = os.path.join(project_folder_path, "Data")
 output_folder = os.path.join(project_folder_path, "Output")
 save_folder = os.path.join(project_folder_path, "SavedModels")
+
+def next_highest_multiple(num:int, base:int):
+    return base*int(math.ceil(max(1, num/base)))
+
+def convert_tcnn_to_pytorch(ckpt, opt):
+    base = 16
+    requires_padding = False
+    if(opt['model'] == "fVSRN"):
+        input_dim = opt['n_features']+opt['num_positional_encoding_terms']*opt['n_dims']*2
+        input_dim_padded = next_highest_multiple(input_dim, base)
+        if(input_dim != input_dim_padded):
+            requires_padding = True
+        
+        output_dim = opt['n_outputs']
+        output_dim_padded = next_highest_multiple(output_dim, base)
+        
+    elif(opt['model'] == "AMGSRN"):
+        input_dim = opt['n_features']*opt['n_grids']
+        input_dim_padded = next_highest_multiple(input_dim, base)
+        if(input_dim != input_dim_padded):
+            requires_padding = True
+        
+        output_dim = opt['n_outputs']
+        output_dim_padded = next_highest_multiple(output_dim, base)
+    
+    else:
+        #print(f"Currently we do not support converting model type {opt['model']} to pure PyTorch.")
+        quit()
+    opt['requires_padded_feats'] = requires_padding  
+        
+    layer_weight_shape = []
+    
+    first_layer_shape = [opt['nodes_per_layer'], input_dim_padded]
+    layer_weight_shape.append(first_layer_shape)
+    
+    for i in range(opt['n_layers']-1):
+        layer_shape = [opt['nodes_per_layer'], opt['nodes_per_layer']]
+        layer_weight_shape.append(layer_shape)
+        
+    last_layer_shape = [output_dim_padded,opt['nodes_per_layer']]
+    layer_weight_shape.append(last_layer_shape)
+    
+    weights = ckpt['state_dict']['decoder.params']
+    new_weights = []
+    current_weight_index = 0
+    for i in range(len(layer_weight_shape)):
+        #print(f"Layer {i}: {layer_weight_shape[i]}")
+        
+        weight_shape_this_layer = layer_weight_shape[i]        
+        num_weights_this_layer = weight_shape_this_layer[0]*weight_shape_this_layer[1]
+        
+        if(current_weight_index+num_weights_this_layer > weights.shape[0]):
+            #print(f"Number of expected weights {current_weight_index+num_weights_this_layer} is larger than the number of weights saved {weights.shape}.")
+            quit()
+        this_layer_weights = weights[current_weight_index:current_weight_index+num_weights_this_layer].clone()
+        this_layer_weights = this_layer_weights.reshape(weight_shape_this_layer)
+        new_weights.append(this_layer_weights)
+        current_weight_index += num_weights_this_layer
+        
+    del ckpt['state_dict']['decoder.params']
+    for i in range(len(new_weights)):
+        if(i == len(new_weights) - 1):
+            # In the last layer, we can actually prune the unused weights when
+            # moving back to PyTorch
+            name = f"decoder.{i}.weight"
+            ckpt['state_dict'][name] = new_weights[i][0:1]
+        else:
+            # The first layer is not pruned, even if it was padded, as TCNN learned
+            # to use those padded weights as a bias essentially
+            # All other layers are not changed            
+            name = f"decoder.{i}.linear.weight"
+            ckpt['state_dict'][name] = new_weights[i]
+    
+    return ckpt
 
 def save_model(model,opt):
     folder = create_folder(save_folder, opt["save_name"])
@@ -29,8 +104,18 @@ def load_model(opt, device):
     model = create_model(opt)
     if(not opt['ensemble']):
         ckpt = torch.load(os.path.join(path_to_load, 'model.ckpt.tar'), 
-            map_location = device)    
-        model.load_state_dict(ckpt['state_dict'])
+            map_location = device)   
+
+        if('decoder.params' in ckpt['state_dict']):
+            #print(f"Model was trained with TCNN")
+            try:
+                import tinycudann
+                model.load_state_dict(ckpt['state_dict'])
+            except ImportError:
+                new_ckpt = convert_tcnn_to_pytorch(ckpt, opt)
+                model.load_state_dict(new_ckpt['state_dict'])
+        else:
+            model.load_state_dict(ckpt['state_dict'])
 
     return model
 
