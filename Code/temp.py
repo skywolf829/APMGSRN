e2e5bc9f953e2b0ca25d97a8975bbc820876f1a2
Skylar Wurster <swwurster@gmail.com>
2023-02-28 21:13:34 -0500

    Update temp.py

--- Code/temp.py
+++ Code/temp.py
@@ -305,7 +305,7 @@ def nc_to_raw(name):
     data, _ = nc_to_tensor(os.path.join(data_folder, name))
     data : np.ndarray = data[0,0].cpu().float().numpy()
     with open(os.path.join(data_folder, f"{name.split('.')[0]}.raw"), 'wb') as f:
-        np.save(f,data)    
+        np.save(f,data.flatten())    
     
 if __name__ == '__main__':
 
